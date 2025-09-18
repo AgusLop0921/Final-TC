25622f4621fe88fd5817d2b773d00a2d79b20288
Agus Lopez <aguslopez@Aguss-MacBook-Pro.local>
2025-09-18 18:22:54 -0300

    test: agrega programas de prueba para if/else, while y for

---
+++ samples/final/final_if_while_for.c
@@ -0,0 +1,22 @@
+int main() {
+    int a = 3;
+    int b = 5;
+    int i = 0;
+    int j;
+
+    if (a < b) {
+        a = a + 1;
+    } else {
+        b = b - 1;
+    }
+
+    while (i < 3) {
+        i = i + 1;
+    }
+
+    for (j = 0; j < 3; j = j + 1) {
+        j = j + 2;
+    }
+
+    return;
+}
